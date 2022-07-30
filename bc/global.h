e26a1353f355c469f2050272893cb92fdbde8fc9
Thomas DiModica <ricinwich@yahoo.com>
2022-07-30 00:46:12 -0600

    Also fix this here.

--- bc/global.h
+++ bc/global.h
@@ -112,9 +112,9 @@ EXTERN int n_history;
 
 #if defined(LIBEDIT)
 /* LIBEDIT data */
-EditLine *edit INIT(NULL);
-History  *hist;
-HistEvent histev;
+EXTERN EditLine *edit INIT(NULL);
+EXTERN History  *hist;
+EXTERN HistEvent histev;
 #endif
 
 /* "Condition code" -- false (0) or true (1) */
